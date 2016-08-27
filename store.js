79390bb67a20613493d42ce57c9198dd27929bef
Patrick Bentley <pbentley07@gmail.com>
2016-08-27 11:43:21 -0400

    Created Click Complete Functionality and Adding Items to Database

--- store.js
+++ store.js
@@ -16,7 +16,39 @@ ListStore = {
     return items
   },
 
-  loadItems: function() {},
-  addItem: function(itemDescription) {},
-  toggleCompleteness: function(itemId) {}
+  loadItems: function() {
+    var loadRequest = $.ajax({
+    type: 'GET',
+    url: "https://listalous.herokuapp.com/lists/Pbentley912/"
+    })
+    loadRequest.done(function(dataFromServer) {
+      items = dataFromServer.items
+      notifyComponents()
+    })
+  },
+  addItem: function(itemDescription) {
+    var creationRequest = $.ajax({
+      type: 'POST',
+      url: "http://listalous.herokuapp.com/lists/Pbentley912/items",
+      data: { description: itemDescription, completed: false }
+    })
+    creationRequest.done(function(itemDataFromServer) {
+    items.push(itemDataFromServer)
+    notifyComponents()
+    })
+  },
+  toggleCompleteness: function(itemId) {
+    var item = findItemById(itemId)
+    var currentCompletedValue = item.completed
+
+    var updateRequest = $.ajax({
+      type: 'PUT',
+      url: "https://listalous.herokuapp.com/lists/Pbentley912/items/" + itemId,
+      data: { completed: !currentCompletedValue }
+    })
+    updateRequest.done(function(itemData) {
+      item.completed = itemData.completed
+      notifyComponents()
+    })
+  }
 }
